ef713dde423a9578f86912bfc089b36689a85540
Andrea Richiardi <a.richiardi.work@gmail.com>
2014-08-05 20:21:25 +0200

    Added equality function with scale for big numbers. Tests now pass.

--- src/sicp/chap1_examples.clj
+++ src/sicp/chap1_examples.clj
@@ -14,7 +14,6 @@
   (< (m/abs (- (m/expt guess 2) x)) 0.001))
 
 (defn sqrt-iter [guess x]
-  (println guess x)
   (if (good-enough? guess x)
     guess
     (sqrt-iter (improve guess x) x)))
@@ -25,16 +24,25 @@
 
 ;; My take to the algorithm looking at page 87 and at section "Comparing Floating-point Numbers"
 ;; of http://gettingclojure.wikidot.com/cookbook:numbers
-(defn equality-with-difference-fn [epsilon]
+(defn equality-comparison-fn [epsilon]
   "Returns a function for evaluating the equality of its two arguments.
   The equality check is a simple comparison between their difference and the epsilon
-  given to difference-equal-fn. The returned fn doesn't work with big numbers."
+  given to this one. The returned fn doesn't work with big numbers."
   (fn [x y]
     (< (m/abs (- x y)) epsilon)))
 
+(defn equality-comparison-with-scale-fn [epsilon]
+  "Returns a function for evaluating the equality of its two arguments.
+  The equality check is a simple comparison between their difference and the epsilon
+  given to this one."
+  (fn [x y]
+    (let [scale (if (and (not (zero? x)) (not (zero? y))) (m/abs x) 1)]
+      (println scale)
+      (< (m/abs (- x y)) (* scale epsilon)))))
+
 (def equal-to?
   "Comparing two numbers using equality-with-difference-fn with epsilon = 0.0001."
-  (equality-with-difference-fn 0.001))
+  (equality-comparison-with-scale-fn 0.0001))
 
 (defn- average*
   "Calculates the average of two or more numbers."
@@ -46,7 +54,6 @@
 
 (defn- sqrt-iter* [& {:keys [guess x]}]
   "Guesses the sqrt of x."
-  (println guess x)
   (cond (equal-to? x (m/expt guess 2)) guess
     (>= guess x) "Error, stack overflow foreseen."
     :else (sqrt-iter* :x x :guess (improve* :x x :guess guess))))
@@ -54,12 +61,12 @@
 (defn sqrt* [x]
   (sqrt-iter* :x x :guess 1.0))
 
-
 (t/deftest chap1-examples
-  (t/is (equal-to? (+ 0.1 0.1 0.1 0.1 0.1 0.1) (* 6 0.1)))
-  (t/is (not (equal-to? 1.23456e38 (* 1.23456 (m/expt 10 38))))) ; doesn't work with big numbers
+  (t/is (equal-to? (+ 0.1 0.1 0.1 0.1 0.1 0.1) (* 6 0.1)) "Small number equality test.")
+  (t/is (equal-to? 1.23456e38 (* 1.23456 (m/expt 10 38))) "Big number equality test.") ; doesn't work with big numbers
   (t/is (equal-to? 2.5 (average* [3 2])) "Average of [3 2]")
   (t/is (equal-to? 2 (average* [3 2 1])) "Average of [3 2 1]")
   (t/is (equal-to? 6.1 (average* [3.7 4.1 9.3 12.4 1])) "Average of [3.7 4.1 9.3 12.4 1]")
   (t/is (equal-to? 1.4142157 (sqrt 2)) "Sqrt of 2 should be equal to what?")
+  (t/is (equal-to? 1.111107556e19 (sqrt* 1.23456e38)))
   (t/is (equal-to? 1.4142157 (sqrt* 2)) "Sqrt (custom) of 2 should be equal to what?"))
